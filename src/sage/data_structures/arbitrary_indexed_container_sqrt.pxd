645f8fbbd505471f5419fa066858783428387c03
Avery Li <averyli408@gmail.com>
2026-03-13 12:10:50 -0700

    docstrings added

--- src/sage/data_structures/arbitrary_indexed_container_sqrt.pxd
+++ src/sage/data_structures/arbitrary_indexed_container_sqrt.pxd
@@ -11,15 +11,15 @@ cdef class Arbitrary_Indexed_Container_Sqrt:
     cdef Py_ssize_t _length
     cdef dict _key_to_position
 
-    cdef list _regions
-    cdef list _regions_key_values
+    cdef list _region_values
+    cdef list _region_keys
     cdef list _region_size
 
     cdef Py_ssize_t _sqrtval
     cdef Py_ssize_t _num_regions
     cdef Py_ssize_t _reinitialize_ratio
 
-    cdef bint _lazy_reinitialize_required
+    cdef bint _lazy_reinitialize_flag
 
     cdef void _initialize_container(self, list key_array, list value_array)
 
--- src/sage/data_structures/arbitrary_indexed_container_sqrt.pyx
+++ src/sage/data_structures/arbitrary_indexed_container_sqrt.pyx
@@ -1,8 +1,46 @@
 # sage_setup: distribution = sagemath-categories
 
+
+"""
+Possible changes
+- prefix sums (depending on access/update frequencies)
+"""
+
+
 cdef class Arbitrary_Indexed_Container_Sqrt:
 
     def __init__(self, key_array=None, value_array=None):
+        """
+        Initialize a sqrt-decomposed indexed container.
+
+        The container stores key/value pairs while maintaining the order
+        of insertion. Internally the data is partitioned into regions of
+        size approximately ``sqrt(n)`` to allow efficient range queries
+        and insertions.
+
+        INPUT:
+
+        - ``key_array`` -- list of keys (optional)
+        - ``value_array`` -- list of values corresponding to ``key_array``
+
+        If both arrays are provided, the container is initialized with
+        those entries.
+
+        EXAMPLES::
+
+            sage: from sage.data_structures.arbitrary_indexed_container_sqrt import Arbitrary_Indexed_Container_Sqrt
+            sage: keys = [(0,0),(1,1),(2,2)]
+            sage: vals = [10,20,30]
+            sage: c = Arbitrary_Indexed_Container_Sqrt(keys, vals)
+            sage: len(c)
+            3
+
+        TESTS::
+
+            sage: c = Arbitrary_Indexed_Container_Sqrt()
+            sage: len(c)
+            0
+        """
 
         self._sqrtval = 0
         self._num_regions = 0
@@ -10,19 +48,48 @@ cdef class Arbitrary_Indexed_Container_Sqrt:
 
         self._key_to_position = {}
 
-        self._regions = []
-        self._regions_key_values = []
+        self._region_values = []                  # 2d array storing values
+        self._region_keys = []              # 2d array storing keys
         self._region_size = []
 
         # Whether or not the container needs to be reinitialized
         # Reinitialization required when container imbalanced and
         # and on __getitem__(slice) or insert
         # append, _initialize_container, reinitialize_container, __len__, index, __getitem__(index)
-        self._lazy_reinitialize_required = False 
+        self._lazy_reinitialize_flag = False 
 
         self._initialize_container(key_array=key_array, value_array=value_array)
 
     cdef void _initialize_container(self, list key_array, list value_array):
+        """
+        Internal method to construct the container from arrays.
+
+        The data is partitioned into regions of size approximately
+        ``sqrt(n)`` and mappings from keys to their region position
+        are created.
+
+        INPUT:
+
+        - ``key_array`` -- list of keys
+        - ``value_array`` -- list of values
+
+        EXAMPLES::
+
+            sage: keys = [(0,0),(1,1),(2,2)]
+            sage: vals = [1,2,3]
+            sage: c = Arbitrary_Indexed_Container_Sqrt(keys, vals)
+            sage: c[(1,1)]
+            2
+
+        TESTS::
+
+            sage: keys = [(0,0),(0,0)]
+            sage: vals = [1,2]
+            sage: Arbitrary_Indexed_Container_Sqrt(keys, vals)
+            Traceback (most recent call last):
+            ...
+            AssertionError: Key array and value array lengths must match
+        """
 
         if (key_array is None) != (value_array is None):
             raise AssertionError("Arrays must both be None or both not None")
@@ -32,7 +99,7 @@ cdef class Arbitrary_Indexed_Container_Sqrt:
             return
 
         if len(key_array) != len(value_array):
-            raise AssertionError("Lengths must match")
+            raise AssertionError("Key array and value array lengths must match")
 
         cdef Py_ssize_t i, region_length
 
@@ -45,8 +112,8 @@ cdef class Arbitrary_Indexed_Container_Sqrt:
         for i in range(self._length):
 
             if i % region_length == 0:
-                self._regions.append([])
-                self._regions_key_values.append([])
+                self._region_values.append([])
+                self._region_keys.append([])
                 self._region_size.append(0)
                 self._num_regions += 1
 
@@ -58,18 +125,42 @@ cdef class Arbitrary_Indexed_Container_Sqrt:
             region_idx = self._num_regions - 1
             offset = self._region_size[region_idx]
 
-            self._regions[region_idx].append(value_array[i])
-            self._regions_key_values[region_idx].append(key)
+            self._region_values[region_idx].append(value_array[i])
+            self._region_keys[region_idx].append(key)
             self._region_size[region_idx] += 1
 
             self._key_to_position[key] = (region_idx, offset)
         
-        self._lazy_reinitialize_required = False
+        self._lazy_reinitialize_flag = False
 
     def __len__(self):
         return self._length
 
     cpdef void reinitialize_container(self):
+        """
+        Rebuild the internal region structure.
+
+        This method reconstructs the container so that regions are
+        balanced again. It is used when regions become too large
+        after many insertions or when accessing and the lazy
+        rebuild flag has been set.
+
+        EXAMPLES::
+
+            sage: c = Arbitrary_Indexed_Container_Sqrt([(0,0)], [1])
+            sage: for i in range(20):
+            ....:     c.append((i+1,i+1), i)
+            sage: c.reinitialize_container()
+            sage: len(c) > 0
+            True
+
+        TESTS::
+
+            sage: c = Arbitrary_Indexed_Container_Sqrt([(0,0)], [1])
+            sage: c.reinitialize_container()
+            sage: len(c)
+            1
+        """
 
         cdef Py_ssize_t i, j, idx = 0
 
@@ -78,22 +169,49 @@ cdef class Arbitrary_Indexed_Container_Sqrt:
 
         for i in range(self._num_regions):
             for j in range(self._region_size[i]):
-                key_array[idx] = self._regions_key_values[i][j]
-                value_array[idx] = self._regions[i][j]
+                key_array[idx] = self._region_keys[i][j]
+                value_array[idx] = self._region_values[i][j]
                 idx += 1
 
         self._length = 0
         self._sqrtval = 0
         self._num_regions = 0
 
-        self._regions.clear()
+        self._region_values.clear()
         self._region_size.clear()
-        self._regions_key_values.clear()
+        self._region_keys.clear()
         self._key_to_position.clear()
 
         self._initialize_container(key_array, value_array)
 
     cpdef int index(self, object key):
+        """
+        Return the integer position of a key in the container.
+
+        INPUT:
+
+        - ``key`` -- key stored in the container
+
+        OUTPUT:
+
+        - integer index of the key in the ordered container
+
+        EXAMPLES::
+
+            sage: keys = [(0,0),(1,1),(2,2)]
+            sage: vals = [10,20,30]
+            sage: c = Arbitrary_Indexed_Container_Sqrt(keys, vals)
+            sage: c.index((1,1))
+            1
+
+        TESTS::
+
+            sage: c = Arbitrary_Indexed_Container_Sqrt([(0,0)], [5])
+            sage: c.index((1,1))
+            Traceback (most recent call last):
+            ...
+            LookupError
+        """
 
         if key not in self._key_to_position:
             raise LookupError(f"{key} not found")
@@ -108,6 +226,31 @@ cdef class Arbitrary_Indexed_Container_Sqrt:
         return idx + offset
 
     def __getitem__(self, object key):
+        """
+        Retrieve a value or a slice of values.
+
+        If ``key`` is a stored key, the corresponding value is returned.
+        If ``key`` is a slice with keys as ``start`` and ``stop``,
+        a list of values in that range is returned.
+
+        EXAMPLES::
+
+            sage: keys = [(0,0),(1,1),(2,2)]
+            sage: vals = [5,6,7]
+            sage: c = Arbitrary_Indexed_Container_Sqrt(keys, vals)
+            sage: c[(1,1)]
+            6
+            sage: c[(0,0):(2,2)]
+            [5, 6]
+
+        TESTS::
+
+            sage: c = Arbitrary_Indexed_Container_Sqrt([(0,0)], [1])
+            sage: c[(1,1)]
+            Traceback (most recent call last):
+            ...
+            LookupError
+        """
         cdef Py_ssize_t start_region_idx, start_region_internal_idx, stop_region_idx, stop_region_internal_idx
 
         if not isinstance(key, slice):
@@ -115,10 +258,10 @@ cdef class Arbitrary_Indexed_Container_Sqrt:
                 raise LookupError(f"{key} not found")
 
             region_idx, offset = self._key_to_position[key]
-            return self._regions[region_idx][offset]
+            return self._region_values[region_idx][offset]
         else:
             # Check if container should be rebuilt first
-            if self._lazy_reinitialize_required:
+            if self._lazy_reinitialize_flag:
                 self.reinitialize_container()
 
             result = []
@@ -149,18 +292,18 @@ cdef class Arbitrary_Indexed_Container_Sqrt:
             #   2. start region == stop region
             if start_region_idx < stop_region_idx:
                 # Add values in start region
-                result.extend(self._regions[start_region_idx][start_region_internal_idx:])
+                result.extend(self._region_values[start_region_idx][start_region_internal_idx:])
                 
                 # Add values in regions between start and stop regions
                 for i in range(start_region_idx + 1, stop_region_idx):
-                    result.extend(self._regions[i])
+                    result.extend(self._region_values[i])
                 
                 # Add values in stop region
-                result.extend(self._regions[stop_region_idx][:stop_region_internal_idx])
+                result.extend(self._region_values[stop_region_idx][:stop_region_internal_idx])
 
             else:
                 # start and stop region are same, get subarray of that region
-                result = self._regions[start_region_idx][start_region_internal_idx:stop_region_internal_idx]
+                result = self._region_values[start_region_idx][start_region_internal_idx:stop_region_internal_idx]
             
             # Return stepped result
             if key.step is None or key.step == 1:
@@ -169,18 +312,63 @@ cdef class Arbitrary_Indexed_Container_Sqrt:
                 return result[::key.step]
 
     def __setitem__(self, object key, object value):
+        """
+        Set the value associated with a key.
+
+        If the key already exists its value is updated.
+        Otherwise the key/value pair is appended to the container.
+
+        EXAMPLES::
+
+            sage: c = Arbitrary_Indexed_Container_Sqrt([(0,0)], [1])
+            sage: c[(0,0)] = 5
+            sage: c[(0,0)]
+            5
+
+        TESTS::
+
+            sage: c = Arbitrary_Indexed_Container_Sqrt()
+            sage: c[(1,1)] = 7
+            sage: c[(1,1)]
+            7
+        """
 
         # If key already exists, set the item
         if key in self._key_to_position:
 
             region_idx, offset = self._key_to_position[key]
-            self._regions[region_idx][offset] = value
+            self._region_values[region_idx][offset] = value
             return
 
         # Otherwise, append it to the end
         self.append(key, value)
 
     cpdef object get_contiguous_array(self):
+        """
+        Return a flattened representation of the container.
+
+        OUTPUT:
+
+        - tuple ``(array, mapping)``
+        - ``array`` -- list of values in order
+        - ``mapping`` -- dictionary mapping keys to indices
+
+        EXAMPLES::
+
+            sage: c = Arbitrary_Indexed_Container_Sqrt([(0,0),(1,1)], [10,20])
+            sage: arr, mp = c.get_contiguous_array()
+            sage: arr
+            [10, 20]
+            sage: mp[(1,1)]
+            1
+
+        TESTS::
+
+            sage: c = Arbitrary_Indexed_Container_Sqrt()
+            sage: arr, mp = c.get_contiguous_array()
+            sage: arr
+            []
+        """
 
         cdef Py_ssize_t idx = 0
         cdef Py_ssize_t i, j
@@ -190,30 +378,53 @@ cdef class Arbitrary_Indexed_Container_Sqrt:
 
         for i in range(self._num_regions):
             for j in range(self._region_size[i]):
-                key = self._regions_key_values[i][j]
-                array[idx] = self._regions[i][j]
+                key = self._region_keys[i][j]
+                array[idx] = self._region_values[i][j]
                 mapping[key] = idx
                 idx += 1
 
         return array, mapping
 
     cpdef void append(self, object key, object value):
+        """
+        Append a key/value pair to the end of the container.
+
+        INPUT:
+
+        - ``key`` -- unique key
+        - ``value`` -- associated value
+
+        EXAMPLES::
+
+            sage: c = Arbitrary_Indexed_Container_Sqrt()
+            sage: c.append((0,0), 10)
+            sage: c[(0,0)]
+            10
+
+        TESTS::
+
+            sage: c = Arbitrary_Indexed_Container_Sqrt([(0,0)], [1])
+            sage: c.append((0,0), 2)
+            Traceback (most recent call last):
+            ...
+            AssertionError
+        """
 
         if key in self._key_to_position:
             raise AssertionError(f"{key} already exists")
 
-        if len(self._regions) == 0:
+        if len(self._region_values) == 0:
 
-            self._regions.append([])
-            self._regions_key_values.append([])
+            self._region_values.append([])
+            self._region_keys.append([])
             self._region_size.append(0)
             self._num_regions += 1
 
         region_idx = self._num_regions - 1
         offset = self._region_size[region_idx]
 
-        self._regions[region_idx].append(value)
-        self._regions_key_values[region_idx].append(key)
+        self._region_values[region_idx].append(value)
+        self._region_keys[region_idx].append(key)
 
         self._key_to_position[key] = (region_idx, offset)
 
@@ -224,19 +435,37 @@ cdef class Arbitrary_Indexed_Container_Sqrt:
         if self._region_size[region_idx] > self._reinitialize_ratio * self._sqrtval:
             # Indicate that reinialization is required now
             # that the data structure has become inefficient
-            self._lazy_reinitialize_required = True
+            self._lazy_reinitialize_flag = True
     
     cpdef void extend(self, list key_array, list value_array):
         """
-        Append multiple key/value pairs directly into the last region.
-        Marks the container for lazy reinitialization if the region grows too large.
+        Append multiple key/value pairs to the container.
+
+        The elements are appended directly to the last region.
+        If the region becomes too large the container will be
+        lazily reinitialized.
 
         INPUT:
-            key_array   -- list of keys
-            value_array -- list of corresponding values
 
-        Raises AssertionError if any key already exists.
+        - ``key_array`` -- list of keys
+        - ``value_array`` -- list of values
+
+        EXAMPLES::
+
+            sage: c = Arbitrary_Indexed_Container_Sqrt()
+            sage: c.extend([(0,0),(1,1)], [10,20])
+            sage: c[(1,1)]
+            20
+
+        TESTS::
+
+            sage: c = Arbitrary_Indexed_Container_Sqrt([(0,0)], [1])
+            sage: c.extend([(0,0)], [5])
+            Traceback (most recent call last):
+            ...
+            AssertionError
         """
+
         cdef Py_ssize_t n, i, region_idx, offset
         n = len(key_array)
         if n != len(value_array):
@@ -248,9 +477,9 @@ cdef class Arbitrary_Indexed_Container_Sqrt:
                 raise AssertionError(f"{key_array[i]} already exists")
 
         # Make sure at least one region exists
-        if len(self._regions) == 0:
-            self._regions.append([])
-            self._regions_key_values.append([])
+        if len(self._region_values) == 0:
+            self._region_values.append([])
+            self._region_keys.append([])
             self._region_size.append(0)
             self._num_regions += 1
 
@@ -259,8 +488,8 @@ cdef class Arbitrary_Indexed_Container_Sqrt:
         offset = self._region_size[region_idx]
 
         # Extend the last region directly
-        self._regions[region_idx].extend(value_array)
-        self._regions_key_values[region_idx].extend(key_array)
+        self._region_values[region_idx].extend(value_array)
+        self._region_keys[region_idx].extend(key_array)
         self._region_size[region_idx] += n
         self._length += n
 
@@ -271,16 +500,45 @@ cdef class Arbitrary_Indexed_Container_Sqrt:
 
         # Mark lazy reinitialization if region grew too large
         if self._region_size[region_idx] > self._reinitialize_ratio * self._sqrtval:
-            self._lazy_reinitialize_required = True
+            self._lazy_reinitialize_flag = True
 
     cpdef void insert(self, Py_ssize_t idx, object key, object value):
+        """
+        Insert a key/value pair at a given index.
+
+        The container locates the region containing the index
+        and inserts the element there.
+
+        INPUT:
+
+        - ``idx`` -- integer position
+        - ``key`` -- unique key
+        - ``value`` -- associated value
+
+        EXAMPLES::
+
+            sage: c = Arbitrary_Indexed_Container_Sqrt([(0,0),(2,2)], [1,3])
+            sage: c.insert(1,(1,1),2)
+            sage: c[(1,1)]
+            2
+            sage: c.index((1,1))
+            1
+
+        TESTS::
+
+            sage: c = Arbitrary_Indexed_Container_Sqrt([(0,0)], [1])
+            sage: c.insert(5,(1,1),2)
+            Traceback (most recent call last):
+            ...
+            AssertionError
+        """
 
         cdef Py_ssize_t region_idx = 0
         cdef Py_ssize_t region_start = 0
         cdef Py_ssize_t insert_offset
 
         # Check if container should be rebuilt first
-        if self._lazy_reinitialize_required:
+        if self._lazy_reinitialize_flag:
             self.reinitialize_container()
 
         if idx < 0 or idx > self._length:
@@ -303,8 +561,8 @@ cdef class Arbitrary_Indexed_Container_Sqrt:
 
         insert_offset = idx - region_start
 
-        self._regions[region_idx].insert(insert_offset, value)
-        self._regions_key_values[region_idx].insert(insert_offset, key)
+        self._region_values[region_idx].insert(insert_offset, value)
+        self._region_keys[region_idx].insert(insert_offset, key)
 
         self._region_size[region_idx] += 1
         self._length += 1
@@ -313,11 +571,11 @@ cdef class Arbitrary_Indexed_Container_Sqrt:
         cdef Py_ssize_t j
         for j in range(insert_offset, self._region_size[region_idx]):
 
-            k = self._regions_key_values[region_idx][j]
+            k = self._region_keys[region_idx][j]
             self._key_to_position[k] = (region_idx, j)      # Update offset to new index
 
         # Check if data structure is inefficient
         if self._region_size[region_idx] > self._reinitialize_ratio * self._sqrtval:
             # Indicate that reinialization is required now
             # that the data structure has become inefficient
-            self._lazy_reinitialize_required = True
+            self._lazy_reinitialize_flag = True
